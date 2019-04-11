c67adacc5bda8bcefc8c8e56ca789acc6b5f0aeb
Your Name <you@example.com>
2019-04-11 17:30:39 -0300

    updates

--- src/_modules/header/header.js
+++ src/_modules/header/header.js
@@ -6,15 +6,23 @@ var Header = function() {
     var body = $('body');
     var menuOpen = $('.header__hamburguer');
     var menuClose = $('.header__nav__close');
+    var logo = $('.header__logo');
 
     menuOpen.on('click', function(){
-        header.addClass('-open');
-        body.addClass('-hideOverflow');
+        header.toggleClass('-open');
+        body.toggleClass('-hideOverflow');
     });
 
-    menuClose.on('click', function(){
-        header.removeClass('-open');
-        body.removeClass('-hideOverflow');
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1) {
+            header.addClass('-changeColor');
+            logo.addClass('-changeColor');
+        }
+
+        if ($(this).scrollTop() < 1) {
+            header.removeClass('-changeColor');
+            logo.removeClass('-changeColor');
+        }
     });
 };
 
